4d2b5a66937d248994adb9cea8bae95247ed11e0
anusha <anushaca1995@gmail.com>
2025-04-30 22:44:11 +0100

    unit test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+beforeEach(() => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve([
+          {
+            UserID: 1,
+            DisplayName: 'John Cena',
+            UserType: 'Actor',
+            Age: 30,
+          },
+        ]),
+    })
+  );
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+test('renders the app and fetches users on load', async () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const johnCena = await screen.findByText((content, node) =>
+    node.textContent === 'John Cena'
+  );
+
+  expect(johnCena).toBeInTheDocument();
 });
